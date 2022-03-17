3f0ba94cda79a7afd525b1c6e91445ca7711600d
w3schools-test <test@w3schools.com>
2022-03-17 13:48:36 +0100

    1 file modified

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 const nodemon = require('nodemon');
 
-nodemon({ script: 'index.js' })
+nodemon({ script: 'myfirst.js' })
   .on('start', console.clear)
   .on('restart', console.clear)
   .on('quit', () => {
